74657cd6763e31be2c9c24de0f194ea5f098be30
MADHAV BAHL <madhavbahl10@gmail.com>
2017-09-30 23:25:47 +0530

    Set up the server

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+
+const port = process.env.PORT || 8080;
+
+var app = express();
+
+app.set('view engine','html');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',(req,res) => {
+  req.render('index.html');
+});
+
+app.listen(port, () => {
+  console.log(`App is up on port ${port}`);
+});
